32cd0760e8d0e9b303c30d3e9e615b7cffa009f8
Tanya Tomchuk <ttp31.1995@gmail.com>
2015-08-26 09:32:59 +0300

    + include.php

---
+++ include.php
@@ -0,0 +1,23 @@
+<?php header('Content-type: text/html; charset=utf-8');?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title>Подключение файлов</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+</head>
+
+<body>
+<?php
+include('inc/heder.php');
+require('inc/body.php');
+require('inc/footer.php');
+?>
+</body>
+</html>
