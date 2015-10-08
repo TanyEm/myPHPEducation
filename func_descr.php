7dfd5ef22ea02c99ec4818199e9d3d200188765e
Томчук Татьяна <ttp31.1995@gmail.com>
2015-10-09 00:30:50 +0300

    addition

--- func_descr.php
+++ func_descr.php
@@ -5,3 +5,40 @@
  * Date: 08.10.2015
  * Time: 23:35
  */
+
+
+function func1()
+{
+    echo 'func1: Функция работает<br>';
+}
+
+function func2($p1,$p2)
+{
+    $d = $p1 / $p2;
+    echo "func2: $p1 / $p2 = $d<br>";
+}
+
+function func3($p1,$p2)
+{
+    return $p1 + $p2;
+}
+
+function func4()
+{
+    $a = array('one','two','three');
+    return $a;
+}
+
+function func5($p1, $p2=15, $p3 = 10)
+{
+    $sum = $p1 + $p2 + $p3;
+    echo "func5: $p1 + $p2 + $p3 = $sum<br>";
+}
+
+function func6($var)
+{
+    $var3 = 5;
+    echo 'func6: $var = '.$var.'<br>';
+    echo 'func6: $var3 = '.$var3.'<br>';
+}
+
--- function.php
+++ function.php
@@ -5,3 +5,64 @@
  * Date: 08.10.2015
  * Time: 23:37
  */
+header('Content-type: text/html; charset=utf-8');?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title>Cycle foreach</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+</head>
+
+<body>
+<?php
+include('func_descr.php');
+
+func1();
+
+echo '<br>';
+
+func2(12,6);
+func2(6,12);
+
+$a = 8;
+$b = 4;
+
+func2($a,$b);
+
+echo '<br>';
+
+$c = func3(6,4);
+
+echo '$c = '.$c.'<br>';
+
+echo func3(7,8);
+
+echo '<br>';
+
+echo '<pre>';
+print_r(func4());
+echo '</pre>';
+
+func5(5);
+func5(5,2,3);
+func5(5,2);
+
+echo '<br>';
+
+$var2 = 2;
+$var3 = 3;
+
+func6(5);
+
+echo '$var3 = '.$var3.'<br><br>';
+
+?>
+</body>
+</html>
