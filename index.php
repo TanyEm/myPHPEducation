4a8e5f0b30abdd77c94e5c21814f14854882584d
Denis Podlesnykh <denniselite@live.com>
2015-10-23 07:50:22 +0300

    Code style fix : index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<?php header('Content-type: text/html; charset=utf-8')
+<?php
+header('Content-type: text/html; charset=utf-8');
 /*
  * Created by PhpStorm.
  * User: Татьяна
  * Date: 20.07.2015
  * Time: 0:27
  */
-;?>
+?>
 <!DOCTYPE html>
 
 <html>
@@ -19,31 +20,31 @@
 <body>
 <?php
 echo '<h1>Индексные массивы</h1>';
-$lang[]='HTML';
-$lang[]='CSS';
-$lang[]='PHP';
-$lang[]='JAVA';
+$lang[] = 'HTML';
+$lang[] = 'CSS';
+$lang[] = 'PHP';
+$lang[] = 'JAVA';
 echo '<br>';
-echo $lang[0].'<br>';
+echo $lang[0] . '<br>';
 echo '<br>';
-echo $lang[1].'<br>';
+echo $lang[1] . '<br>';
 echo '<br>';
-echo $lang[2].'<br>';
+echo $lang[2] . '<br>';
 echo '<br>';
-echo $lang[3].'<br>';
+echo $lang[3] . '<br>';
 echo '<pre>';
 print_r($lang);
 echo '<pre>';
 
 echo '<br><br>';
-$lang2[3]='perl';
-$lang2[7]='piton';
-$lang2[10]='ruby';
-$lang2[14]='JS';
+$lang2[3] = 'perl';
+$lang2[7] = 'piton';
+$lang2[10] = 'ruby';
+$lang2[14] = 'JS';
 echo '<pre>';
 print_r($lang2);
 echo '<pre>';
-$lang2[]='C#';
+$lang2[] = 'C#';
 echo '<pre>';
 print_r($lang2);
 echo '<pre>';
@@ -53,7 +54,7 @@ echo '<pre>';
 print_r($lang3);
 echo '<pre>';
 echo "Каскадные таблицы стилей: $lang3[10] <br>";
-echo 'каскадные таблицы стилей: '.$lang3[11].'<br>';
+echo 'каскадные таблицы стилей: ' . $lang3[11] . '<br>';
 ?>
 </body>
 </html>
